56d74a2de8cb664a5b28bc49cb0f82b133ac75ec
Zubeyde-sayin-kutlug <szubeyde@hotmail.com>
2021-07-18 01:02:58 +0200

    Coding Form.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,12 +2,35 @@ import React, { Component } from "react";
 import "../Form.css";
 
 export default class Form extends Component {
+  state = { text: "", phone: "" };
+  onInputChange = (e) => {
+    //console.log(e.target.name, e.target.value);
+    // if (e.target.name === "text") {
+    //   this.setState({ text: e.target.value });
+    // } else if (e.target.name === "phone") {
+    //   this.setState({ phone : e.target.value });
+    // }
+    this.setState({ [e.target.name]: e.target.value });
+    console.log("Last State:", this.state);
+  };
   render() {
     return (
       <div className="formField">
         <form>
-          <input name="name" id="name" placeholder="Enter a name..."></input>
-          <input name="phone" id="phone" placeholder="Enter a phone..."></input>
+          <input
+            onChange={this.onInputChange}
+            value={this.state.text}
+            name="text"
+            id="text"
+            placeholder="Enter a name..."
+          ></input>
+          <input
+            onChange={this.onInputChange}
+            value={this.state.phone}
+            name="phone"
+            id="phone"
+            placeholder="Enter a phone..."
+          ></input>
           <button>Add</button>
         </form>
       </div>
